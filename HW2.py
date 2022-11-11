e78209124a35a3a7a508ff26dab7ff94a5baf0be
AihoTab <114133261+AihoTab@users.noreply.github.com>
2022-11-12 01:43:56 +0300

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -4,3 +4,12 @@ lst = [random.randint(0, 99) for i in range(random.randint(5, 15))]
 print(lst)
 random.shuffle(lst)
 print(lst)
+
+# Решение Анатолия
+# i = [int(x) for x in input().split()]
+# for j, item in enumerate(i):
+#     if item % 2 == 0 and j > 0:
+#         i[j], i[j-1] = i[j-1], i[j]
+#     elif item % 2 != 0 and j < len(i)-1:
+#         i[j], i[j+1] = i[j+1], i[j]
+# print(*i, sep=" ", end=" ")
